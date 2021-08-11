746d93dfcf0b08b17ff82a8e547b426f339c6c31
Vinicius <viniciusgribas@gmail.com>
2021-08-11 20:45:58 -0300

    Função para realizar a leitura de dados do banco

--- features.py
+++ features.py
@@ -8,3 +8,9 @@ def db_conect():
     """
     função para realizar conexão com o banco de dados.
     """
+
+def db_read():
+    """
+    Função para realizar a leitura de dados do banco
+    de dados.
+    """
